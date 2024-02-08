70a7b912e5bd1c43dab6c1f5d2b40056ed53eb38
Joe Murray <jmurray45@gmail.com>
2024-02-08 11:47:46 -0500

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by JAM on 2/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
