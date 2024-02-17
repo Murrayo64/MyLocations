1947b612bb6e25f2026295f13fee27a72022d280
Joe Murray <jmurray45@gmail.com>
2024-02-16 21:46:25 -0500

    Pg 679

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by JAM on 2/15/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
