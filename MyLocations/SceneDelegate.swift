baae9bf07321f3a1f9cf88eef1842662cd8036b4
Joe Murray <jmurray45@gmail.com>
2024-02-16 22:17:53 -0500

    CH 28 start

--- MyLocations/SceneDelegate.swift
+++ MyLocations/SceneDelegate.swift
@@ -22,6 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let controller = navController.viewControllers.first as! CurrentLocationViewController
             controller.managedObjectContext = managedObjectContext
         }
+        listenForFatalCoreDataNotifications()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -97,6 +98,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    // MARK: - Helper Methods
+    func listenForFatalCoreDataNotifications() {
+        // 1
+        NotificationCenter.default.addObserver(
+            forName: dataSaveFailedNotification,
+            object: nil,
+            queue: OperationQueue.main
+        ) { _ in
+            // 2
+            let message = """
+            There was a fatal error in the app and it cannot continue.
+            
+            Press OK to terminate the app. Sorry for the inconvenience.
+            """
+            // 3
+            let alert = UIAlertController(
+                title: "Internal Error",
+                message: message,
+                preferredStyle: .alert)
+            
+            // 4
+            let action = UIAlertAction(title: "OK", style: .default) {
+                _ in
+                let exception = NSException(
+                    name: NSExceptionName.internalInconsistencyException,
+                    reason: "Fatal Core Data error",
+                userInfo: nil)
+                exception.raise()
+            }
+            alert.addAction(action)
+            
+            // 5
+            let tabController = self.window!.rootViewController!
+            tabController.present(
+                alert,
+                animated: true,
+                completion: nil)
         }
     }
 
