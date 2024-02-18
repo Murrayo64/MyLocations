79759df4885116d454e808331b36089d09a9036d
Joe Murray <jmurray45@gmail.com>
2024-02-18 00:41:11 -0500

    pg 754

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -26,6 +26,7 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var addPhotoLabel: UILabel!
+    @IBOutlet var imageHeight: NSLayoutConstraint!
     var image: UIImage?
     var coordinate = CLLocationCoordinate2D(
         latitude: 0,
@@ -114,6 +115,8 @@ class LocationDetailsViewController: UITableViewController {
         imageView.image = image
         imageView.isHidden = false
         addPhotoLabel.text = ""
+        imageHeight.constant = 260
+        tableView.reloadData()
     }
     
     // MARK: - Navigation
