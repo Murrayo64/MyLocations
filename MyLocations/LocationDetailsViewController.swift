8e6e612a4e3138e9011453fa654751bef8e8ef66
Joe Murray <jmurray45@gmail.com>
2024-02-17 23:11:11 -0500

    pg 749

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -146,7 +146,7 @@ class LocationDetailsViewController: UITableViewController {
         if indexPath.section == 0 && indexPath.row == 0 {
             descriptionTextView.becomeFirstResponder()
         } else if indexPath.section == 1 && indexPath.row == 0 {
-            choosePhotoFromLibrary()
+            pickPhoto()
         }
     }
     
@@ -222,5 +222,40 @@ extension LocationDetailsViewController:
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func pickPhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        let alert = UIAlertController(
+            title: nil,
+            message: nil,
+            preferredStyle: .actionSheet)
+        
+        let actCancel = UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil)
+        alert.addAction(actCancel)
+        
+        let actPhoto = UIAlertAction(
+            title: "Take Photo",
+            style: .default,
+            handler: nil)
+        alert.addAction(actPhoto)
+        
+        let actLibrary = UIAlertAction(
+            title: "Chooes From Library",
+            style: .default,
+            handler: nil)
+        alert.addAction(actLibrary)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
